75e0b3922af81613834b603fced3b17ef742bf52
MauroGomez <mauro@buzzhives.com>
2021-02-02 17:47:49 -0300

    Cleanup

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@ import PengineClient from './PengineClient';
 import Board from './Board';
 
 class Game extends React.Component {
-  
+
   pengine;
 
   constructor(props) {
@@ -20,13 +20,13 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
     // No action on click if game has ended, we are waiting for game status, or clicked cell is not empty.
     if (this.state.status !== '?' || this.state.waiting) {
       return;
     }
     const squaresS = PengineClient.stringify(this.state.squares);
-    const queryS = "put(" + PengineClient.stringify(this.state.xIsNext ? 'X' : 'O') + ", " + i + ", " + squaresS + ", BoardRes), gameStatus(BoardRes, Status)";
+    const queryS = "put(" + PengineClient.stringify(this.state.xIsNext ? "X" : "O") + "," + i + "," + squaresS + ",BoardRes),"
+      + "gameStatus(BoardRes, Status)";
     this.setState({
       waiting: true
     });
@@ -37,33 +37,13 @@ class Game extends React.Component {
           xIsNext: !this.state.xIsNext,
           status: response["Status"],
           waiting: false
-        })
+        });
       } else {
         this.setState({
           waiting: false
-        })
+        });
       }
     });
-  }
-
-  test(query) {
-    this.pengine.query(query, (success, response, more) => {
-      console.log("Answer to " + query + ":");
-      console.log(success ? response : "false");
-      more && console.log("more");
-    });
-  }
-
-  testNext() {
-    this.pengine.next((success, response, more) => {
-      console.log("Next solution:");
-      console.log(success ? response : "false");
-      more && console.log("more");
-    });
-  }
-
-  testError() {
-    this.pengine.query("0 is X", () => {});
   }
 
   render() {
@@ -78,54 +58,17 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
+          <Board
             squares={this.state.squares}
             onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <div>
-            <button onClick={() => this.test("X=a")}>
-              test
-            </button>
-            <button onClick={() => this.test("member(X, [a, b, c])")}>
-              test multi-solutions
-            </button>
-            <button onClick={() => this.test("X\\=a")}>
-              test fail
-            </button>
-            <button onClick={() => this.testNext()}>
-              test next
-            </button>
-            <button onClick={() => this.testError()}>
-              test error
-            </button>
-            </div>
         </div>
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 export default Game;
