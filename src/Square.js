db67fe3e4f2bf73411006d7a73e3fc6f75463554
MauroGomez <mauro@buzzhives.com>
2021-03-04 12:30:44 -0300

    Alert on square click

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={this.props.onClick}>
+            <button className="square" onClick={() => alert('click')}>
                 {this.props.value}
             </button>
         );
