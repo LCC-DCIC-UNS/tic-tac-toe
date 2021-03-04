3bc57bd5fe1953f333e505aaef9e3d38cdfcecbf
MauroGomez <mauro@buzzhives.com>
2021-03-04 16:12:24 -0300

    Square with state to remember it was clicked

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        };
+      }
+
     render() {
         return (
-            <button className="square" onClick={() => alert('click')}>
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
