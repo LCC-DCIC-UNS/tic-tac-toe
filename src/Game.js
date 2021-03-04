5a931e9695c99d759df6510d96be92be1634c413
MauroGomez <mauro@buzzhives.com>
2021-03-04 12:07:23 -0300

    Switch from using " to '.

--- src/Game.js
+++ src/Game.js
@@ -6,10 +6,9 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill("-"),
+      squares: Array(9).fill('-'),
       xIsNext: true,
-      status: '?',  // values: 'X' (X is the winner), 'O' (O is the winner), 'T' (tie), '?' (game in progress)
-      waiting: false
+      status: '?'  // values: 'X' (X is the winner), 'O' (O is the winner), 'T' (tie), '?' (game in progress)
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -27,41 +26,40 @@ class Game extends React.Component {
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] !== "-" && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (squares[a] !== '-' && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
     }
-    if (squares.every(square => square !== "-")) {
+    if (squares.every(square => square !== '-')) {
       return 'T';
     }
     return '?';
   }
 
   handleClick(i) {
-    // No action on click if game has ended, we are waiting for game status, or clicked cell is not empty.
-    if (this.state.status !== '?' || this.state.waiting || this.state.squares[i] !== '-') {
+    // No action on click if game has ended or clicked cell is not empty.
+    if (this.state.status !== '?' || this.state.squares[i] !== '-') {
       return;
     }
     // Make the move, and update game status
     const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? "X" : "O";  
+    squares[i] = this.state.xIsNext ? 'X' : 'O';  
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
-      status: this.gameStatus(squares),
-      waiting: false
+      status: this.gameStatus(squares)
     });
   }
 
   render() {
     const status = this.state.status;
     let statusText;
-    if (status === "?") {
-      statusText = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    } else if (status === "T") {
-      statusText = "Tie!"
+    if (status === '?') {
+      statusText = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    } else if (status === 'T') {
+      statusText = 'Tie!'
     } else {
-      statusText = "Winner: " + status;
+      statusText = 'Winner: ' + status;
     }
     return (
       <div className="game">
