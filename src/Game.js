903108f77bb7f20f6840abf7662e91f8d30bbaae
MauroGomez <mauro@buzzhives.com>
2021-03-04 12:00:09 -0300

    Switch from using " to '.

--- src/Game.js
+++ src/Game.js
@@ -19,25 +19,25 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    // No action on click if game has ended, we are waiting for game status, or clicked cell is not empty.
-    if (this.state.status !== '?' || this.state.waiting) {
+    // No action on click if game has ended, clicked cell is not empty, or we are waiting for game status.
+    if (this.state.status !== '?' || this.state.squares[i] !== '-'  || this.state.waiting) {
       return;
     }
     // Build Prolog query to make a move and get the updated game status.
-    // Calls to PengineClient.stringify() are to explicitly quote terms for player and board cells ("X", "Y" and "-")
-    // The query will be like: put("X",0,["-","-","-","-","-","-","-","-","-"],BoardRes),gameStatus(BoardRes, Status) 
+    // Calls to PengineClient.stringify() are to explicitly quote terms for player and board cells ('X', 'Y' and '-')
+    // The query will be like: put('X',0,['-','-','-','-','-','-','-','-','-'],BoardRes),gameStatus(BoardRes, Status) 
     const squaresS = PengineClient.stringify(this.state.squares);
-    const queryS = "put(" + PengineClient.stringify(this.state.xIsNext ? "X" : "O") + "," + i + "," + squaresS + ",BoardRes),"
-      + "gameStatus(BoardRes, Status)";
+    const queryS = 'put(' + PengineClient.stringify(this.state.xIsNext ? 'X' : 'O') + ',' + i + ',' + squaresS + ',BoardRes),'
+      + 'gameStatus(BoardRes, Status)';
     this.setState({
       waiting: true
     });
     this.pengine.query(queryS, (success, response) => {
       if (success) {
         this.setState({
-          squares: response["BoardRes"],
+          squares: response['BoardRes'],
           xIsNext: !this.state.xIsNext,
-          status: response["Status"],
+          status: response['Status'],
           waiting: false
         });
       } else {
