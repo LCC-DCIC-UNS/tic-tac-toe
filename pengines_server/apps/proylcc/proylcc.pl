a05840550babf72d355677cde88a51e98f41b62d
MauroGomez <mauro@buzzhives.com>
2021-04-26 09:50:02 -0300

    Add documentation to Prolog implementation.

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -10,6 +10,7 @@
 %
 % replace(?X, +XIndex, +Y, +Xs, -XsY)
 %
+% XsY es el resultado de reemplazar la ocurrencia de X en la posición XIndex de Xs por Y.
 
 replace(X, 0, Y, [X|Xs], [Y|Xs]).
 
@@ -24,8 +25,17 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
 %
 
 put(Contenido, [RowN, ColN], _PistasFilas, _PistasColumnas, Grilla, NewGrilla, 0, 0):-
+	% NewGrilla es el resultado de reemplazar la fila Row en la posición RowN de Grilla
+	% (RowN-ésima fila de Grilla), por una fila nueva NewRow.
+	
 	replace(Row, RowN, NewRow, Grilla, NewGrilla),
+
+	% NewRow es el resultado de reemplazar la celda Cell en la posición ColN de Row por _,
+	% siempre y cuando Cell coincida con Contenido (Cell se instancia en la llamada al replace/5).
+	% En caso contrario (;)
+	% NewRow es el resultado de reemplazar lo que se que haya (_Cell) en la posición ColN de Row por Conenido.	 
+	
 	(replace(Cell, ColN, _, Row, NewRow),
 	Cell == Contenido 
-		; 
+		;
 	replace(_Cell, ColN, Contenido, Row, NewRow)).
